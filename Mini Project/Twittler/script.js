d143158a9526bfee8b67b80366bf3c87ce8a41a6
Hyeongyu-IM <64323969+Hyeongyu-IM@users.noreply.github.com>
2020-08-30 23:39:32 +0900

    hard...

---
+++ Mini Project/Twittler/script.js
@@ -0,0 +1,44 @@
+// 트윗버튼을 누르면 아이디와 내용을 받아와서 ul에 추가해준다
+// check new tweet을 누르면 랜덤으로 내용을 출력한다.
+// 아이디를 클릭하면 해당 아이디가 작성한 트윗만을 볼수있도록 만든다
+
+
+// let target = document.querySelector("#list");
+// let render = document.querySelector("#render");
+// let tweetBtn = document.querySelector(".tweetBtn")
+// let newtweet = document.querySelector(".checkBtn")
+// let ID = document.querySelector(".username")
+// let text = document.querySelector(".text")
+// let date = document.querySelector(".date")
+
+// let today = new Date()
+
+// function add() {
+//     let comment = document.importNode(render.content, true);
+
+//     ID.innerHTML =
+// }
+
+
+
+
+
+
+
+
+
+
+// let comment = document.importNode(render.content, true);
+// target.appendChild(comment);
+
+
+
+// your code here
+
+// DATA는 이미 작성된 트윗을 표시합니다.
+//console.log(DATA)
+
+// generateNewTweet을 호출할 때마다 새로운 트윗을 생성합니다.
+//console.log(generateNewTweet());
+
+//document.getElementById('test').innerHTML = 'hello twittler, check developer console!';
